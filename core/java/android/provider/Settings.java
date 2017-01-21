4647e11938d65c7443cbd15024370241e8af4886
Alex Cruz <mazdarider23@gmail.com>
2017-01-21 15:48:53 +0100

    Dashboard tile layout config [2/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3962,6 +3962,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Dashboard columns in settings
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
