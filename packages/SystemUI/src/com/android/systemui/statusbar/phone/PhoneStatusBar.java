f27ce17c31da9cbe4610f65f810075196f9ec353
ezio84 <brabus84@gmail.com>
2017-02-25 16:42:52 +0100

    always use automatic text color for card header 1/2
    with so many colors for the header it's not worth it to
    manually select a text color, the automatic color util does a
    good job (using white for the most cases and black when the card
    header is white).

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -514,9 +514,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENT_CARD_BG_COLOR),
                     false, this, UserHandle.USER_ALL);
-            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+            /*mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENT_CARD_TEXT_COLOR),
-                    false, this, UserHandle.USER_ALL);
+                    false, this, UserHandle.USER_ALL);*/
             updateAll();
         }
 
