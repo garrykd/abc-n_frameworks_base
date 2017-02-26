c05a47258de25264f964c55e567869ae3178b8d3
ezio84 <brabus84@gmail.com>
2017-02-27 00:46:19 +0100

    Fix scroll jank/lag when Auto Expand enabled
    make the scroll smooth after you open the panel and
    scroll it the first time
    
    https://developer.android.com/reference/android/support/v7/widget/LinearLayoutManager.html#getExtraLayoutSpace(android.support.v7.widget.RecyclerView.State)
    
    getExtraLayoutSpace
    
    int getExtraLayoutSpace (RecyclerView.State state)
    Returns the amount of extra space that should be laid out by LayoutManager.
    
    By default, LinearLayoutManager lays out 1 extra page of items while smooth scrolling and 0 otherwise. You can override this method to implement your custom layout pre-cache logic.
    
    Note:Laying out invisible elements generally comes with significant performance cost. It's typically only desirable in places like smooth scrolling to an unknown location, where 1) the extra content helps LinearLayoutManager know in advance when its target is approaching, so it can decelerate early and smoothly and 2) while motion is continuous.
    
    Extending the extra layout space is especially expensive if done while the user may change scrolling direction. Changing direction will cause the extra layout space to swap to the opposite side of the viewport, incurring many rebinds/recycles, unless the cache is large enough to handle it.
    
    Parameters
    state	RecyclerView.State
    Returns
    int	The extra space that should be laid out (in pixels).

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -34,12 +34,14 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
@@ -176,8 +178,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                 (CardRecyclerView) mRecentContainer.findViewById(R.id.recent_list);
 
         cardRecyclerView.setHasFixedSize(true);
-
-        LinearLayoutManager llm = new LinearLayoutManager(context);
+        CacheMoreCardsLayoutManager llm = new CacheMoreCardsLayoutManager(context, mWindowManager);
         llm.setReverseLayout(true);
         cardRecyclerView.setLayoutManager(llm);
 
@@ -821,5 +822,29 @@ public class RecentController implements RecentPanelView.OnExitListener,
             }
         }
    }
+
+    private class CacheMoreCardsLayoutManager extends LinearLayoutManager {
+        private Context context;
+        private WindowManager mWindowManager;
+
+        public CacheMoreCardsLayoutManager(Context context, WindowManager windowManager) {
+            super(context);
+            this.context = context;
+            this.mWindowManager = windowManager;
+        }
+
+        @Override
+        protected int getExtraLayoutSpace(RecyclerView.State state) {
+            return getScreenHeight();
+        }
+
+        private int getScreenHeight() {
+            Display display = mWindowManager.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            int screenHeight = size.y;
+            return screenHeight;
+        }
+    }
 
 }
